7d7dbfff5dbdb84aa2485c90db3145a418e8d27b
opilane <opilane@HP-CZC6297B82>
2021-11-25 16:04:04 +0200

    Add project files.

---
+++ PythonApplication4333.py
@@ -0,0 +1,59 @@
+#1 задание
+
+#f=int(input("Введите число от 1 до 9  "))
+#if f>9:
+#    print("Меньше 9 нельзя")
+#else:
+#    a=str("  ^---^")
+#    b=str(" ( o o )")
+#    c=str("  ! = !/)")
+#    print(a*f)
+#    print(b*f)
+#    print(c*f)
+
+#2 задание
+
+#p = int(input("Показатель степени: "))
+#r = int(input("Предел: ")) 
+#s = 1
+#while s ** p <= r:
+#    print(s ** p, end=' ')
+#    s += 1
+ 
+#print("\rПоследнее число,")
+#print(" возведенное в степень:", s - 1)
+
+#4 задание
+
+#k = 1
+#for i in range(0,25,3):
+#    k *=3
+#print(k)
+
+#3 задание
+
+#from random import *
+#max=0
+#min=5
+#ljudi=randint(1,25)
+#for i in range(ljudi):
+#    grade=randint(1,5)
+#    if grade<5:
+#        grade=grade+random()
+#        grade=round(grade)
+#    print(grade, end=" ")
+#    if grade>max:
+#        max=grade
+#    if grade<min:
+#        min=grade
+
+#5 задание
+
+#a=int(input("Сколько котлет у губки боба - "))
+#b=(input("сколько на одну сковородку помещается котлет"))
+#for i in range (a):
+#    c=a//b
+#print(f"Понадобится {c} сковородки")
+
+
+
